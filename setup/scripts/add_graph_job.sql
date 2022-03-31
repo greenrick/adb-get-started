76de9dd3b37b94d9de6d9d962b014d9f22c21894
Marty <martin.gubar@oracle.com>
2022-03-31 16:28:06 -0400

    major updates to add_data

---
+++ setup/scripts/add_graph_job.sql
@@ -0,0 +1,17 @@
+create or replace procedure add_graph_job as 
+begin
+    -- CREATE GRAPH (ASYNC JOB)
+     workshop.write('create async job that creates and populates the graph');
+     begin
+        dbms_scheduler.create_job (
+           job_name             => 'create_graph',
+           job_type             => 'STORED_PROCEDURE',
+           job_action           => 'add_graph',
+           start_date           => current_timestamp,
+           enabled              => true
+           );
+     end;
+
+     
+     workshop.write('adding graph complete.');
+end add_graph_job;
--- setup/workshop-setup.sql
+++ setup/workshop-setup.sql
@@ -2,3 +2,33 @@
    (	execution_time timestamp (6), 
 	    message varchar2(32000 byte)
    ) ;
+
+create or replace view datasets as
+        with all_datasets as
+        (
+            select 
+                a.doc.table_name as table_name,
+                to_number(a.doc.seq) as seq,
+                a.doc.source_uri as source_uri,
+                a.doc.format as format,
+                a.doc.sql as sql,
+                a.doc.post_load_proc as post_load_proc,
+                a.doc.constraints as constraints,
+                a.doc.description as description,
+                a.doc.dependencies as dependencies
+            from external (
+            (
+                doc clob     	   
+            ) 
+            type ORACLE_BIGDATA
+            default directory DATA_PUMP_DIR
+            access parameters (
+                com.oracle.bigdata.fileformat=textfile
+                com.oracle.bigdata.csv.rowformat.fields.terminator='\n'
+            )
+            LOCATION ('https://raw.githubusercontent.com/martygubar/adb-get-started/master/setup/datasets.json')
+            ) a 
+        )
+        select a.* 
+        from all_datasets a
+        ;   
