9cbff2c03c2fcf18784acd752f4c7b4370ede74f
Marty <martin.gubar@oracle.com>
2022-04-07 15:40:46 -0400

    test

--- setup/scripts/test.sql
+++ setup/scripts/test.sql
@@ -1,4 +1,22 @@
 -- Table used for logging operations
-create table workshop_log (	execution_time timestamp (6), message varchar2(32000 byte)) 
+create table workshop_log 
+   (	execution_time timestamp (6), 
+	    message varchar2(32000 byte)
+   ) 
 /
 
+-- Data set listing based on config file on github
+
+create or replace view workshop_datasets as
+    select 
+        a.doc.table_name as table_name,
+        to_number(a.doc.seq) as seq,
+        a.doc.source_uri as source_uri,
+        a.doc.format as format,
+        a.doc.sql as sql,
+        a.doc.post_load_proc as post_load_proc,
+        a.doc.constraints as constraints,
+        a.doc.description as description,
+        a.doc.dependencies as dependencies
+    from ext_datasets a 
+/
